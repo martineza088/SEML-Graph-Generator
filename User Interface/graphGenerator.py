1f79301103a7e9b0334049fb68ca32bb58ffb857
martineza088 <martineza@southwestern.edu>
2025-07-11 01:26:53 -0500

    reading from txt

--- User Interface/graphGenerator.py
+++ User Interface/graphGenerator.py
@@ -15,11 +15,29 @@ graphCases = [[4, 8, 1, 7, 3, 6, 5, 2], [1, 6, 2, 10, 3, 12, 5, 4, 8, 7, 9, 11],
               [1, 12, 2, 15, 3, 28, 4, 26, 5, 21, 6, 25, 7, 27, 9, 8, 11, 10, 23, 22, 20, 19, 14, 13, 17, 16, 18, 24], # 14 vertices
               [1, 36, 2, 33, 3, 18, 4, 23, 5, 22, 6, 35, 7, 28, 8, 32, 9, 34, 31, 30, 27, 26, 11, 10, 15, 14, 13, 12, 25, 24, 17, 16, 20, 19, 21, 29], # 18 vertices
               [6, 9, 11, 3, 10, 5, 12, 7, 1, 2, 4, 8]]
-caseNum = 0
+caseNum = 4
 
 # retrieving graphs from txt file and generating an array that will be used to generate graphs
-
     # graph values 
+filePath1 = r"C:\Users\alyan\Downloads\Research\SEML Research (Summer '24 & '25)\SEML-Graph-Generator\User Interface\testUserInput.txt"
+
+with open(filePath1) as file: # "with" keyword automatically opens the file and closes it closes file after it is read
+    #fileContents = file.read()
+    #print("File Contents: ", fileContents)
+    readLineOutput = []
+    fileLine = file.readline()
+    fileLine2 = file.readline()
+    #while (file.readline()):
+     #   readLineOutput.append(fileLine)
+    #readLineOutput.append(fileLine)
+    readLineOutput.append(fileLine)
+    readLineOutput.append(fileLine2)
+    print("Read a line of file contents: ", str(readLineOutput))
+
+
+
+
+# transform this into an array of file contents (each element is a line in the file)
     # track the value of k !!
 k = 0
 
@@ -199,6 +217,8 @@ for i in range(len(edgeCoordPoints)):
 
 
 # create a single function that generates the whole graph
+def graphGen():
+    return 1
 
 
 # create a loop that iterates through the array generated using values provided in the txt
